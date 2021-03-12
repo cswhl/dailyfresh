a73e09ab60cde981bae8b5efae793a21ee252d10
cswhl <cswhltime@126.com>
2021-03-12 19:19:42 +0000

    2021-3-12

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
-from goods.models import GoodsType
+from django.core.cache import cache
+from goods.models import GoodsType, IndexGoodsBanner, IndexTypeGoodsBanner, IndexPromotionBanner
+from goods.models import Goods, GoodsSKU
 
 # Register your models here.
 
-admin.site.register(GoodsType)
+
+@admin.register(GoodsType, IndexGoodsBanner, IndexTypeGoodsBanner,IndexPromotionBanner)
+class BaseModelAdmin(admin.ModelAdmin):
+    '''自定义模型管理类'''
+
+    def save_model(self, request, obj, form, change):
+        '''新增或更新表中数据时被调用'''
+        super().save_model(request, obj, form, change)
+
+        # 发出任务，让celery worker重新生成首页静态页面
+        from celery_tasks.tasks import generate_static_index_html
+        generate_static_index_html.delay()
+
+        # 清除首页缓存
+        cache.delete('index_page_data')
+
+    def delete_model(self, request, obj):
+        '''删除表中数据时被调用'''
+        super().delete_model(request, obj)
+
+        # 发出任务，让celery worker重新生成首页静态页面
+        from celery_tasks.tasks import generate_static_index_html
+        generate_static_index_html.delay()
+
+
+admin.site.register(Goods)
+admin.site.register(GoodsSKU)
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,7 +1,56 @@
 from django.shortcuts import render
+from django.core.cache import cache
+from django.views.generic import View
+from goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
+from django_redis import get_redis_connection
+
 
 # Create your views here.
-def index(request):
-    '''网站首页'''
 
-    return render(request, 'index.html')
+# http://192.168.18.129:8000/index
+class IndexView(View):
+
+    def get(self, request):
+        '''网站首页'''
+
+        context = cache.get('index_page_data')
+
+        if context is None:
+            print('设置缓存')
+            # 获取商品的种类信息
+            types = GoodsType.objects.all()
+
+            # 获取首页轮播商品信息
+            goods_banners = IndexGoodsBanner.objects.all().order_by('index')
+
+            # 获取首页促销活动信息
+            promotion_banners = IndexPromotionBanner.objects.all().order_by('index')
+
+            # 获取首页分类商品展示信息
+            for type in types:
+                # 获取type种类首页分类商品的图片展示信息
+                image_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1).order_by('index')
+
+                # 获取type种类首页分离额商品的文字展示信息
+                title_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0).order_by('index')
+
+                # 动态给type增加属性，分别保存分类商品的图片展示信息和文字展示信息
+                type.image_banners = image_banners
+                type.title_banners = title_banners
+
+            context = {'types': types,
+                       'goods_banners': goods_banners,
+                       'promotion_banners': promotion_banners}
+
+            cache.set('index_page_data', context, 3600)
+
+        # 从redis中获取购物车信息
+        user = request.user
+        if user.is_authenticated():
+            # 用户已经登录
+            conn = get_redis_connection('default')
+            cart_count = conn.hlen(f'cart_{user.id}')
+
+        context.update(cart_count=cart_count)
+
+        return render(request, 'index.html', context)
